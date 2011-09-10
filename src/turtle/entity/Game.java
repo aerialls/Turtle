0e87ff7667b8b6e3fa13f8578fa7a60b5954fcc3
Julien Brochet <mewt@madalynn.eu>
2011-09-10 18:51:23 +0200

    changed: Game class now extends Observable

--- src/turtle/entity/Game.java
+++ src/turtle/entity/Game.java
@@ -1,8 +1,9 @@
 package turtle.entity;
 
 import java.awt.Color;
+import java.util.Observable;
 
-public class Game
+public class Game extends Observable
 {
     protected Team mTeamA;
     protected Team mTeamB;
