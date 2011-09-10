4489ce87e049e6e6ebeaa2c0aa943ea2b1889069
Julien Brochet <mewt@madalynn.eu>
2011-09-10 20:32:28 +0200

    added license header

--- src/turtle/gui/AbstractWindow.java
+++ src/turtle/gui/AbstractWindow.java
@@ -1,3 +1,12 @@
+/*
+ * This file is part of the Turtle project
+ *
+ * (c) 2011 Julien Brochet <julien.brochet@etu.univ-lyon1.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 package turtle.gui;
 
 import java.util.Observer;
