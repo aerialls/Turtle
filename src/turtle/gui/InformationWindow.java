a1a7d48477651f8ba4940d74cb156ab8ad4570aa
Julien Brochet <mewt@madalynn.eu>
2011-09-10 19:19:27 +0200

    added InformationWindow class

---
+++ src/turtle/gui/InformationWindow.java
@@ -0,0 +1,18 @@
+package turtle.gui;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JFrame;
+
+public class InformationWindow extends JFrame implements Observer
+{
+    public InformationWindow()
+    {
+    }
+
+    @Override
+    public void update(Observable o, Object arg)
+    {
+    }
+}
