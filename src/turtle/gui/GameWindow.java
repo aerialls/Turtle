e38da7d14e39cd8134f1f241ae38b69127647073
Julien Brochet <mewt@madalynn.eu>
2011-09-10 18:59:05 +0200

    added GameWindows class

---
+++ src/turtle/gui/GameWindow.java
@@ -0,0 +1,18 @@
+package turtle.gui;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JFrame;
+
+public class GameWindow extends JFrame implements Observer
+{
+    public GameWindow()
+    {
+    }
+
+    @Override
+    public void update(Observable o, Object arg)
+    {
+    }
+}
