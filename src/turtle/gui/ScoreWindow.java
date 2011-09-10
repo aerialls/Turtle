90a7950bc6d39b2512cec23c3f23e2d90cec5425
Julien Brochet <mewt@madalynn.eu>
2011-09-10 19:14:44 +0200

    added ScoreWindows class

---
+++ src/turtle/gui/ScoreWindow.java
@@ -0,0 +1,18 @@
+package turtle.gui;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JFrame;
+
+public class ScoreWindow extends JFrame implements Observer
+{
+    public ScoreWindow()
+    {
+    }
+
+    @Override
+    public void update(Observable o, Object arg)
+    {
+    }
+}
