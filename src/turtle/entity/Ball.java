32182d5a30ad8e2bfaa0b998b683196d0b030174
Julien Brochet <mewt@madalynn.eu>
2011-09-18 17:57:08 +0200

    added Ball class

---
+++ src/turtle/entity/Ball.java
@@ -0,0 +1,31 @@
+/*
+ * This file is part of the Turtle project
+ *
+ * (c) 2011 Julien Brochet <julien.brochet@etu.univ-lyon1.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package turtle.entity;
+
+import java.awt.geom.Point2D;
+
+/**
+ * Représentation d'un ballon de foot
+ *
+ * @author Julien Brochet <julien.brochet@etu.univ-lyon1.fr>
+ * @since 1.0
+ */
+public class Ball
+{
+    /**
+     * La position du ballon
+     */
+    protected Point2D mPosition;
+
+    public Ball(Point2D position)
+    {
+        mPosition = position;
+    }
+}
