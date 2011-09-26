1fda5a4ef61f589f404e86c4a6d003117034ffe8
Julien Brochet <mewt@madalynn.eu>
2011-09-26 19:51:03 +0200

    added empty TurtleView

---
+++ src/turtle/gui/view/TurtleView.java
@@ -0,0 +1,26 @@
+/*
+ * This file is part of the Turtle project
+ *
+ * (c) 2011 Julien Brochet <julien.brochet@etu.univ-lyon1.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package turtle.gui.view;
+
+import java.awt.Graphics2D;
+import turtle.entity.Turtle;
+
+/**
+ * Représentation de la vue d'un joueur
+ *
+ * @author Julien Brochet <julien.brochet@etu.univ-lyon1.fr>
+ * @since 1.0
+ */
+public class TurtleView implements ViewInterface
+{
+    public static void paint(Turtle turtle, Graphics2D g)
+    {
+    }
+}
