c78949ac45926ef5af85cf9b6ebefedee4d64a30
Julien Brochet <mewt@madalynn.eu>
2011-10-16 14:37:42 +0200

    releasing 1.0-PROTOTYPE-3

--- src/turtle/controller/Kernel.java
+++ src/turtle/controller/Kernel.java
@@ -26,7 +26,7 @@ public class Kernel implements Runnable
     /**
      * Version du programme
      */
-    public static final String VERSION = "1.0-PROTOTYPE";
+    public static final String VERSION = "1.0-PROTOTYPE-3";
 
     /**
      * Si le programme est en mode débug ou non
